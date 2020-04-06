dc2f4ae3ba6e4684750b1573b15b81b6ffa3e5ee
walter.estrada <walter.estrada@CO-IT004982>
2020-04-06 00:57:35 -0500

    Primer commit, proyecto de aprendizaje de Microservicios y Ocelot

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        DatabaseContext db;
+        public UserController()
+        {
+            db = new DatabaseContext();
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+    }
+}
